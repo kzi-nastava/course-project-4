efdc348f7779be5cc6f3c05fdebff05fa82e7e1d
savic-a <anastasijas557@gmail.com>
2022-04-22 16:12:46 +0200

    Implemented tasks for first checkpoint

--- Hospital/Hospital/PatientImplementation/Patient.cs
+++ Hospital/Hospital/PatientImplementation/Patient.cs
@@ -12,21 +12,20 @@ namespace Hospital.PatientImplementation
     class Patient
     {
         string email;
-        List<Appointment> myAppointments;
         HelperClass helper;
+        List<Appointment> allMyAppointments;
 
         public string Email { get { return email; } }
-        public List<Appointment> PatientAppointments { get { return myAppointments; } }
+        public List<Appointment> PatientAppointments { get { return allMyAppointments; } }
 
-        public Patient(string email, List<Appointment> myAppointments, HelperClass helper)
+        public Patient(string email, HelperClass helper)
         {
             this.email = email;
-            this.myAppointments = myAppointments;
             this.helper = helper;
+            this.allMyAppointments = helper.refreshPatientAppointments();
         }
 
         // methods
-
         public void patientMeni()
         {
             // meni
@@ -46,6 +45,8 @@ namespace Hospital.PatientImplementation
                 // my choice
                 if (choice.Equals("1"))
                     this.readOwnAppointments();
+                else if (choice.Equals("2"))
+                    this.createAppointment();
                 else if (choice.Equals("3"))
                     this.updateAppointment();
                 else if (choice.Equals("4"))
@@ -57,14 +58,21 @@ namespace Hospital.PatientImplementation
 
         private void readOwnAppointments()
         {
+            if (this.allMyAppointments.Count == 0)
+            {
+                Console.WriteLine("\nJos uvek nemate zakazan pregled!");
+                return;
+            }
+                
             int i = 1;
             Console.WriteLine("\n\tPREGLEDI");
             Console.Write("--------------------------\n");
-            foreach (Appointment appointment in this.myAppointments)
+            foreach (Appointment appointment in this.allMyAppointments)
             {
                 // check if the appointment is scheduled and has not yet passed
-                if (appointment.GetAppointmentState == Appointment.AppointmentState.Created ||
-                    appointment.GetAppointmentState == Appointment.AppointmentState.Modified)
+                if ((appointment.GetAppointmentState == Appointment.AppointmentState.Created ||
+                    appointment.GetAppointmentState == Appointment.AppointmentState.Modified) &&
+                    appointment.DateExamination > DateTime.Now)
                 {
                     Console.WriteLine(i + ". " + appointment.ToString());
                     i++;
@@ -84,9 +92,10 @@ namespace Hospital.PatientImplementation
                 Console.WriteLine("Unesite broj pregleda za brisanje");
                 Console.Write(">> ");
                 numberAppointment = Console.ReadLine();
-            } while (Int32.Parse(numberAppointment) > this.myAppointments.Count && numberAppointment.Equals("0") && numberAppointment.Contains("-"));
+            } while (Int32.Parse(numberAppointment) > this.allMyAppointments.Count && numberAppointment.Equals("0") 
+            && numberAppointment.Contains("-"));
 
-            Appointment appointmentForDelete = this.myAppointments[Int32.Parse(numberAppointment) - 1];
+            Appointment appointmentForDelete = this.allMyAppointments[Int32.Parse(numberAppointment) - 1];
 
             string filePath = @"..\..\Data\appointments.csv";
             string[] lines = File.ReadAllLines(filePath);
@@ -96,16 +105,19 @@ namespace Hospital.PatientImplementation
                 string id = fields[0];
 
                 if (id.Equals(appointmentForDelete.AppointmentId)) {
+
+                    string deletionDate = DateTime.Now.Month + "/" + DateTime.Now.Day + "/" + DateTime.Now.Year;
+
                     if ((DateTime.Now - appointmentForDelete.DateExamination).TotalDays <= 2)
                     {
-                        lines[i] = id + "," + fields[1] + "," + fields[2] + "," + fields[3] + "," + fields[4] + "," +
-                            fields[5] + "," + (int)Appointment.AppointmentState.DeleteRequest;
+                        lines[i] = id + "," + fields[1] + "," + fields[2] + "," + deletionDate + "," + fields[4] + "," + fields[5] 
+                            + "," + fields[6] + "," + (int)Appointment.AppointmentState.DeleteRequest;
                         Console.WriteLine("Zahtev za brisanje je poslat sekretaru!");
                     }
                     else
                     {
-                        lines[i] = id + "," + fields[1] + "," + fields[2] + "," + fields[3] + "," + fields[4] + "," +
-                            fields[5] + "," + (int)Appointment.AppointmentState.Deleted;
+                        lines[i] = id + "," + fields[1] + "," + fields[2] + "," + deletionDate + "," + fields[4] + "," + fields[5] 
+                            + "," + fields[6] + "," + (int)Appointment.AppointmentState.Deleted;
                         Console.WriteLine("Uspesno ste obrisali pregled!");
                     }
                 }
@@ -115,7 +127,10 @@ namespace Hospital.PatientImplementation
             File.WriteAllLines(filePath, lines);
 
             //refresh data
-            this.myAppointments = helper.refreshPatientAppointments();
+            this.allMyAppointments = helper.refreshPatientAppointments();
+
+            // check number of changed deleted and created appointments
+            this.antiTrolMechanism();
         }
 
         private void updateAppointment()
@@ -128,9 +143,10 @@ namespace Hospital.PatientImplementation
                 Console.WriteLine("Unesite broj pregleda za izmenu");
                 Console.Write(">> ");
                 numberAppointment = Console.ReadLine();
-            } while (Int32.Parse(numberAppointment) > this.myAppointments.Count && numberAppointment.Equals("0") && numberAppointment.Contains("-"));
+            } while (Int32.Parse(numberAppointment) > this.allMyAppointments.Count && numberAppointment.Equals("0") && 
+            numberAppointment.Contains("-"));
 
-            Appointment appointmentForUpdate = this.myAppointments[Int32.Parse(numberAppointment) - 1];
+            Appointment appointmentForUpdate = this.allMyAppointments[Int32.Parse(numberAppointment) - 1];
 
             // update
             string doctorEmail;
@@ -159,19 +175,22 @@ namespace Hospital.PatientImplementation
 
                     if (id.Equals(appointmentForUpdate.AppointmentId))
                     {
+                        string modificationDate = DateTime.Now.Month + "/" + DateTime.Now.Day + "/" + DateTime.Now.Year;
                         DateTime startTime = DateTime.Parse(newStartTime);
                         DateTime newEndTime = startTime.AddMinutes(15);
 
                         if ((DateTime.Now - appointmentForUpdate.DateExamination).TotalDays <= 2)
                         {
-                            lines[i] = id + "," + fields[1] + "," + doctorEmail + "," + newDate + "," + newStartTime + "," +
-                                newEndTime.Hour + ":" + newEndTime.Minute + "," + (int)Appointment.AppointmentState.ChangeRequest;
+                            lines[i] = id + "," + fields[1] + "," + doctorEmail + "," + modificationDate + "," + newDate + "," +
+                                newStartTime + "," + newEndTime.Hour + ":" + newEndTime.Minute + "," + 
+                                (int)Appointment.AppointmentState.ChangeRequest;
                             Console.WriteLine("Zahtev za izmenu je poslat sekretaru!");
                         }
                         else
                         {
-                            lines[i] = id + "," + fields[1] + "," + doctorEmail + "," + newDate + "," + newStartTime + "," +
-                                newEndTime.Hour + ":" + newEndTime.Minute + "," + (int)Appointment.AppointmentState.Modified;
+                            lines[i] = id + "," + fields[1] + "," + doctorEmail + "," + modificationDate + "," + newDate + "," + 
+                                newStartTime + "," + newEndTime.Hour + ":" + newEndTime.Minute + "," + 
+                                (int)Appointment.AppointmentState.Modified;
                             Console.WriteLine("Uspesno ste izvrsili izmenu pregleda!");
                         }
                     }
@@ -181,9 +200,79 @@ namespace Hospital.PatientImplementation
                 File.WriteAllLines(filePath, lines);
 
                 //refresh data
-                this.myAppointments = helper.refreshPatientAppointments();
+                this.allMyAppointments = helper.refreshPatientAppointments();
+
+                // check number of changed deleted and created appointments
+                this.antiTrolMechanism();
             }
 
+        }
+
+        private void createAppointment()
+        {
+            string doctorEmail;
+            string newDate;
+            string newStartTime;
+
+            do
+            {
+                Console.Write("\nUnesite email doktora: ");
+                doctorEmail = Console.ReadLine();
+                Console.Write("Unesite datum (MM/dd/yyyy): ");
+                newDate = Console.ReadLine();
+                Console.Write("Unesite vreme pocetka pregleda (HH:mm): ");
+                newStartTime = Console.ReadLine();
+            } while (!helper.isValidInput(doctorEmail, newDate, newStartTime));
+
+            if (helper.isAppointmentFree(doctorEmail, newDate, newStartTime))
+            {
+                int id = helper.getNewAppointmentId();
+                string schedulingDate = DateTime.Now.Month + "/" + DateTime.Now.Day + "/" + DateTime.Now.Year;
+                DateTime startTime = DateTime.Parse(newStartTime);
+                DateTime newEndTime = startTime.AddMinutes(15);
+
+                string newAppointment = "\n" + id + "," + this.email + "," + doctorEmail + "," + schedulingDate + "," + newDate + "," + 
+                    newStartTime + "," + newEndTime.Hour + ":" + newEndTime.Minute + "," + (int)Appointment.AppointmentState.Created;
+
+                // append new appointment in file
+                string filePath = @"..\..\Data\appointments.csv";
+                File.AppendAllText(filePath, newAppointment);
+
+                // refresh data
+                this.allMyAppointments = helper.refreshPatientAppointments();
+
+                // check number of changed deleted and created appointments
+                this.antiTrolMechanism();
+            }
+        }
+
+        private void antiTrolMechanism()
+        {
+            int changed = 0;
+            int deleted = 0;
+            int created = 0;
+
+            foreach (Appointment appointment in allMyAppointments) {
+                if ((DateTime.Now - appointment.SchedulingDate).TotalDays <= 30) {
+                    if (appointment.GetAppointmentState == Appointment.AppointmentState.Created)
+                        created += 1;
+                    else if (appointment.GetAppointmentState == Appointment.AppointmentState.Modified)
+                        changed += 1;
+                    else if (appointment.GetAppointmentState == Appointment.AppointmentState.Deleted)
+                        deleted += 1;
+                }
+            }
+
+            if (changed > 4)
+                Console.WriteLine("\nU proteklih 30 dana previse puta ste izmenili termin.\nPristup aplikaciji Vam je sada blokiran!");
+            else if (deleted > 4)
+                Console.WriteLine("\nU proteklih 30 dana previse puta ste obrisali termin.\nPristup aplikaciji Vam je sada blokiran!");
+            else if (created > 8)
+                Console.WriteLine("\nU proteklih 30 dana previse puta ste kreirali termin.\nPristup aplikaciji Vam je sada blokiran!");
+            else
+                return;
+            helper.blockAccessApplication(this.email);
+            System.Environment.Exit(0); //exit from application
         }
     }
 }
