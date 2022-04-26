ccd793276733eb16cb418d7b1ed8377e00855d47
savic-a <anastasijas557@gmail.com>
2022-04-26 16:21:57 +0200

    Add action csv file
    This file stores all the actions that any patient has performed on an appointment.

---
+++ Hospital/Hospital/Model/Action.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.PatientImplementation;
+
+namespace Hospital.Model
+{
+    class Action
+    {
+        public enum ActionState
+        {
+            Created = 1,
+            Modified = 2,
+            Deleted = 3
+        }
+
+        Patient patient;
+        DateTime actionDate;
+        ActionState actionState;
+
+        public Patient AppointmentId { get { return patient; } }
+        public DateTime ActionDate { get { return actionDate; } }
+        public ActionState GetActionState { get { return actionState; } }
+    }
+}
