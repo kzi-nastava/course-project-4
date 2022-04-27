5170b6ddcb82327617def8af2922f904ec207183
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-04-27 09:22:11 +0200

    Add new attributes

--- Hospital/Hospital/Model/Appointment.cs
+++ Hospital/Hospital/Model/Appointment.cs
@@ -16,44 +16,61 @@ namespace Hospital.Model
             ChangeRequest = 4,
             DeleteRequest = 5
         }
+        public enum TypeOfTerm
+        {
+            Examination = 1,
+            Operation = 2
+        }
 
         private string id;
         private string patientEmail;
         private string doctorEmail;
-        private DateTime schedulingDate;
-        private DateTime dateExamination;
+        private DateTime dateAppointment;
         private DateTime startTime;
         private DateTime endTime;
         private AppointmentState state;
+        private int roomNumber;
+        private TypeOfTerm term;
+
 
         public string AppointmentId { get { return id; } }
         public string PatientEmail { get { return patientEmail; } }
         public string DoctorEmail { get { return doctorEmail; } }
-        public DateTime SchedulingDate { get { return schedulingDate; } }
-        public DateTime DateExamination { get { return dateExamination; } }
+        public DateTime DateAppointment { get { return dateAppointment; } }
         public DateTime StartTime { get { return startTime; } }
         public DateTime EndTime { get { return endTime; } }
         public AppointmentState GetAppointmentState { get { return state; } }
 
-        public Appointment(string id, string patientEmail, string doctorEmail, DateTime schedulingDate,
-        DateTime dateExamination, DateTime start, DateTime end, AppointmentState state)
+        public int RoomNumber { get { return roomNumber; } }
+        public TypeOfTerm GetTypeOfTerm { get { return term;} }
+
+        public Appointment(string id, string patientEmail, string doctorEmail,
+        DateTime dateAppointment, DateTime start, DateTime end, AppointmentState state, int roomNumber, TypeOfTerm term)
         {
             this.id = id;
             this.patientEmail = patientEmail;
             this.doctorEmail = doctorEmail;
-            this.schedulingDate = schedulingDate;
-            this.dateExamination = dateExamination;
+            this.dateAppointment = dateAppointment;
             this.startTime = start;
             this.endTime = end;
             this.state = state;
+            this.roomNumber = roomNumber;
+            this.term = term;
         }
 
         public override string ToString()
         {
             return "Doktor: " + this.doctorEmail + 
-                " Datum: " + this.dateExamination.Day + "/" + this.dateExamination.Month + "/" + this.dateExamination.Year +
+                " Datum: " + this.dateAppointment.Day + "/" + this.dateAppointment.Month + "/" + this.dateAppointment.Year +
                 " Pocetak: " + this.startTime.Hour + ":" + this.startTime.Minute +
                 " Kraj: " + this.endTime.Hour + ":" + this.endTime.Minute;
         }
+        public string ToStringDisplayForDoctor(int serialNumber)
+        {
+            return String.Format("|{0,5}|{1,10}|{2,10}|{3,10}|{4,10}|{5,10}|{6,10}", serialNumber, this.patientEmail, this.dateAppointment.Month + "/" + this.dateAppointment.Day + "/" + this.dateAppointment.Year, this.startTime.Hour + ":" + this.startTime.Minute, this.endTime.Hour + ":" + this.endTime.Minute, this.roomNumber, this.GetTypeOfTerm); 
+              
+        }
+
+
     }
 }
