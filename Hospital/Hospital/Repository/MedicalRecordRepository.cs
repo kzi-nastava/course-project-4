5c3faf2d25aa67f9e8a05c97f89074f5c9de178a
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-04-30 20:27:27 +0200

    Added classes for health record

---
+++ Hospital/Hospital/Repository/MedicalRecordRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Model;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Hospital.Repository
+{
+    class MedicalRecordRepository
+    {
+        public List<MedicalRecord> Load()
+        {
+            List<MedicalRecord> allMedicalRecords = new List<MedicalRecord>();
+            using (TextFieldParser parser = new TextFieldParser(@"..\..\Data\medicalRecords.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(";");
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    string idHealthRecord = fields[0];
+                    string anamnesis = fields[1];
+                    string referralToDoctor = fields[2];
+
+                    MedicalRecord newMedicalRecord = new MedicalRecord(idHealthRecord, anamnesis, referralToDoctor);
+                    allMedicalRecords.Add(newMedicalRecord);
+
+                }
+            }
+            return allMedicalRecords;
+        }
+
+    }
+}
---
+++ Hospital/Hospital/Service/MedicalRecordService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Repository;
+using Hospital.Model;
+
+namespace Hospital.Service
+{
+    class MedicalRecordService
+    {
+        private MedicalRecordRepository _medicalRecordRepository;
+        private List<MedicalRecord> _medicalRecords;
+
+        public MedicalRecordService()
+        {
+            this._medicalRecordRepository = new MedicalRecordRepository();
+            this._medicalRecords = _medicalRecordRepository.Load();
+
+        }
+
+        public List<MedicalRecord> GetMedicalRecords { get { return _medicalRecords; } }
+    }
+}
