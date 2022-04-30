3d1ef23864634ae511416e5d5f808e9514c15b6a
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-04-30 21:52:53 +0200

    Added classes for medical record and their attributes

---
+++ Hospital/Hospital/Service/MedicalRecordService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Repository;
+using Hospital.Model;
+namespace Hospital.Service
+{
+    class MedicalRecordService
+    {
+        private MedicalRecordRepository medicalRecordRepository;
+        private List<MedicalRecord> medicalRecords;
+
+        public MedicalRecordService()
+        {
+            this.medicalRecordRepository = new MedicalRecordRepository();
+            this.medicalRecords = medicalRecordRepository.Load();
+
+        }
+
+        public List<MedicalRecord> GetMedicalRecords { get { return medicalRecords; } }
+    }
+}
