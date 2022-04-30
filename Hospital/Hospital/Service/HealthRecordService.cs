f54a40a7a7f2065255c58921d0e205b062be882a
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-04-30 20:36:56 +0200

    Added new function for health record service

--- Hospital/Hospital/Service/HealthRecordService.cs
+++ Hospital/Hospital/Service/HealthRecordService.cs
@@ -5,26 +5,53 @@ using System.Text;
 using System.Threading.Tasks;
 using Hospital.Repository;
 using Hospital.Model;
+using System.IO;
+
 namespace Hospital.Service
 {
     class HealthRecordService
     {
-        private HealthRecordRepository healthRecordRepository;
-        private UserRepository userRepository;
-        private List<HealthRecord> healthRecords;
-        private List<User> users;
+        private HealthRecordRepository _healthRecordRepository;
+        private UserRepository _userRepository;
+        private List<HealthRecord> _healthRecords;
+        private List<User> _users;
 
 
         public HealthRecordService()
         {
-            healthRecordRepository = new HealthRecordRepository();
-            userRepository = new UserRepository();
-            healthRecords = healthRecordRepository.Load();
-            users = userRepository.Load();
+            _healthRecordRepository = new HealthRecordRepository();
+            _userRepository = new UserRepository();
+            _healthRecords = _healthRecordRepository.Load();
+            _users = _userRepository.Load();
 
         }
 
-        public List<HealthRecord> GetHealthRecords { get{ return healthRecords; } }
+        public List<HealthRecord> GetHealthRecords { get{ return _healthRecords; } }
+
+        public void UpdateHealthRecord(HealthRecord healthRecord)
+        {
+            
+            string filePath = @"..\..\Data\healthRecords.csv";
+            string[] lines = File.ReadAllLines(filePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] fields = lines[i].Split(new[] { ',' });
+                string id = fields[0];
+
+                if (id.Equals(healthRecord.IdHealthRecord))
+                {
+
+                    lines[i] = id + "," + healthRecord.EmailPatient + "," + healthRecord.PatientHeight.ToString() + "," + healthRecord.PatientWeight.ToString() + "," + healthRecord.PreviousIllnesses + "," + healthRecord.Allergen
+                        + "," + healthRecord.BloodType;
+                    Console.WriteLine("Uspesno ste izmenili zdravstveni karton");
+
+
+                }
+            }
+            // saving changes
+            File.WriteAllLines(filePath, lines);
+        }
         
     }
 }
