16570c7412432a977321963b1819712023f7a967
Tadija Šebez <sebez.sv4.2020@uns.ac.rs>
2022-04-30 10:42:52 +0200

    Implement saving rooms to csv file

--- Hospital/Hospital/Repository/RoomRepository.cs
+++ Hospital/Hospital/Repository/RoomRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,13 @@ namespace Hospital.Repository
 {
     public class RoomRepository
     {
+        private static string filePath = @"..\..\Data\rooms.csv";
+
         public List<Room> Load()
         {
             List<Room> allRooms = new List<Room>();
 
-            using (TextFieldParser parser = new TextFieldParser(@"..\..\Data\rooms.csv"))
+            using (TextFieldParser parser = new TextFieldParser(filePath))
             {
                 parser.TextFieldType = FieldType.Delimited;
                 parser.SetDelimiters(",");
@@ -32,6 +35,19 @@ namespace Hospital.Repository
             }
 
             return allRooms;
+        }
+
+        public void Save(List<Room> allRooms)
+        {
+            string[] lines = new string[allRooms.Count];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Room room = allRooms[i];
+                lines[i] = room.Id + "," + room.Name + "," + ((int)room.Type).ToString();
+            }
+            
+            File.WriteAllLines(filePath, lines);
         }
     }
 }
