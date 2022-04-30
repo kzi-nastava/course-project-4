3473cfb85ebaec3c0a33c5ef30c05f1de4163bcc
Tadija Šebez <sebez.sv4.2020@uns.ac.rs>
2022-04-30 14:22:38 +0200

    Implement Equipment class

---
+++ Hospital/Hospital/Model/Equipment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Model
+{
+    public class Equipment
+    {
+        public enum TypeOfEquipment
+        {
+            ExaminationEquipment = 1,
+            OperationEquipment = 2,
+            Furniture = 3,
+            HallwayEquipment = 4
+        }
+
+        private string id;
+        private string name;
+        private TypeOfEquipment type;
+        private int quantity;
+
+        public string Id { get { return id; } }
+        public string Name { get { return name; } }
+        public TypeOfEquipment Type { get { return type; } }
+        public int Quantity { get { return quantity; } }
+
+        public string TypeStr
+        {
+            get
+            {
+                switch (type)
+                {
+                    case TypeOfEquipment.ExaminationEquipment:
+                        return "Oprema za preglede";
+                    case TypeOfEquipment.OperationEquipment:
+                        return "Oprema za operacije";
+                    case TypeOfEquipment.Furniture:
+                        return "Sobni namestaj";
+                    case TypeOfEquipment.HallwayEquipment:
+                    default:
+                        return "Oprema za hodnike";
+                }
+            }
+        }
+
+        public Equipment(string id, string name, TypeOfEquipment type, int quantity)
+        {
+            this.id = id;
+            this.name = name;
+            this.type = type;
+            this.quantity = quantity;
+        }
+    }
+}
