a2f8e16af213ad42f9ca917b353da03ca43828ce
Tadija Šebez <sebez.sv4.2020@uns.ac.rs>
2022-04-30 10:15:25 +0200

    Implement RoomRepository class

---
+++ Hospital/Hospital/Repository/RoomRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Model;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Hospital.Repository
+{
+    public class RoomRepository
+    {
+        public List<Room> Load()
+        {
+            List<Room> allRooms = new List<Room>();
+
+            using (TextFieldParser parser = new TextFieldParser(@"..\..\Data\rooms.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+
+                    string id = fields[0];
+                    string name = fields[1];
+                    Room.TypeOfRoom type = (Room.TypeOfRoom)int.Parse(fields[2]);
+                    
+                    Room room = new Room(id, name, type);
+                    allRooms.Add(room);
+                }
+            }
+
+            return allRooms;
+        }
+    }
+}
