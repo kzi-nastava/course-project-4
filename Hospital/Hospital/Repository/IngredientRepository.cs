710a93e5d7d4b95ec3f9b950381c2c3e26344241
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-05-09 17:01:00 +0200

    Added classes for working with drug ingredients

---
+++ Hospital/Hospital/Repository/IngredientRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Model;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Hospital.Repository
+{
+    class IngredientRepository
+    {
+        public List<Ingredient> Load()
+        {
+            List<Ingredient> allIngredients = new List<Ingredient>();
+            using (TextFieldParser parser = new TextFieldParser(@"..\..\Data\ingredients.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    string id = fields[0];
+                    string ingredientName = fields[1];
+
+                    Ingredient newIngredient = new Ingredient(id, ingredientName);
+                    allIngredients.Add(newIngredient);
+                }
+            }
+            return allIngredients;
+        }
+    }
+}
