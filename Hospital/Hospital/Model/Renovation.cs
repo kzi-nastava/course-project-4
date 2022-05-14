c33acc8f11ef7263b0619d64cc4dc9a9bff825ba
Tadija Šebez <sebez.sv4.2020@uns.ac.rs>
2022-05-14 03:00:19 +0200

    Implement Renovation model class

---
+++ Hospital/Hospital/Model/Renovation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Model
+{
+    public class Renovation
+    {
+        private string _id;
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private string _roomId;
+
+        public string Id { get { return _id; } }
+        public DateTime StartDate { get { return _startDate; } }
+        public DateTime EndDate { get { return _endDate; } }
+        public string RoomId { get { return _roomId; } }
+
+        public Renovation(string id, DateTime startDate, DateTime endDate, string roomId)
+        {
+            this._id = id;
+            this._startDate = startDate;
+            this._endDate = endDate;
+            this._roomId = roomId;
+        }
+    }
+}
