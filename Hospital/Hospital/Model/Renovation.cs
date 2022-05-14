3f302b9e5d1337f522d000c22a8869f4dbfdef5b
Tadija Šebez <sebez.sv4.2020@uns.ac.rs>
2022-05-14 03:22:10 +0200

    Add attribute storing if renovation is performed

--- Hospital/Hospital/Model/Renovation.cs
+++ Hospital/Hospital/Model/Renovation.cs
@@ -12,18 +12,21 @@ namespace Hospital.Model
         private DateTime _startDate;
         private DateTime _endDate;
         private string _roomId;
+        private bool _active;
 
         public string Id { get { return _id; } }
         public DateTime StartDate { get { return _startDate; } }
         public DateTime EndDate { get { return _endDate; } }
         public string RoomId { get { return _roomId; } }
+        public bool IsActive { get { return _active; } }
 
-        public Renovation(string id, DateTime startDate, DateTime endDate, string roomId)
+        public Renovation(string id, DateTime startDate, DateTime endDate, string roomId, bool active)
         {
             this._id = id;
             this._startDate = startDate;
             this._endDate = endDate;
             this._roomId = roomId;
+            this._active = active;
         }
     }
 }
