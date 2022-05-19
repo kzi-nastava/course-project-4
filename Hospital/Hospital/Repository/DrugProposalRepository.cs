94d8ed38abc7bda12bb4e25f34b1480553fc5d20
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-05-19 13:35:34 +0200

    Added drug verification classes

---
+++ Hospital/Hospital/Repository/DrugProposalRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Model;
+using Microsoft.VisualBasic.FileIO;
+using Hospital.Service;
+
+namespace Hospital.Repository
+{
+    class DrugProposalRepository
+    {
+        private IngredientService _ingredientService;
+
+        public DrugProposalRepository()
+        {
+            _ingredientService = new IngredientService();
+        }
+
+
+        public List<DrugProposal> Load()
+        {
+            List<DrugProposal> allDrugProposals = new List<DrugProposal>();
+            using (TextFieldParser parser = new TextFieldParser(@"..\..\Data\drugProposals.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters("*");
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    string idProposal = fields[0];
+                    string drugName = fields[1];
+                    List<string> ingredientIds = fields[2].Split(';').ToList();
+                    List<Ingredient> ingredients = new List<Ingredient>();
+                    foreach (string id in ingredientIds)
+                    {
+                        ingredients.Add(_ingredientService.Get(id));
+                    }
+                    DrugProposal.Status status = (DrugProposal.Status)int.Parse(fields[3]);
+                    string comment = fields[4];
+                    DrugProposal newDrugProposal = new DrugProposal(idProposal, drugName, ingredients, status, comment);
+                    allDrugProposals.Add(newDrugProposal);
+                }
+            }
+            return allDrugProposals;
+        }
+    }
+}
---
+++ Hospital/Hospital/Service/DrugProposalService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Repository;
+using Hospital.Model;
+
+namespace Hospital.Service
+{
+    class DrugProposalService
+    {
+        private DrugProposalRepository _drugProposalRepository;
+        private List<DrugProposal> _drugProposals;
+
+
+        public DrugProposalService()
+        {
+            this._drugProposalRepository = new DrugProposalRepository();
+            this._drugProposals = _drugProposalRepository.Load();
+        }
+
+        public List<DrugProposal> DrugProposals { get { return _drugProposals; }set { _drugProposals = value; } }
+    }
+}
