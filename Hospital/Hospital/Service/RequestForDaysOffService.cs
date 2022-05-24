0cee92686f58b2033966a8a8301f339975a3f871
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-05-24 02:46:29 +0200

    Implemented new classes for the request for days off

---
+++ Hospital/Hospital/Service/RequestForDaysOffService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Model;
+using Hospital.Repository;
+
+namespace Hospital.Service
+{
+    class RequestForDaysOffService
+    {
+        private RequestForDaysOffRepository _requestForDaysOffRepository;
+        private List<RequestForDaysOff> _requestsForDaysOff;
+
+
+        public RequestForDaysOffService()
+        {
+            this._requestForDaysOffRepository = new RequestForDaysOffRepository();
+            this._requestsForDaysOff = this._requestForDaysOffRepository.Load();
+            
+        }
+
+
+        public List<RequestForDaysOff> RequirementsForDaysOff { get { return _requestsForDaysOff; } set { _requestsForDaysOff = value; } }
+    }
+}
