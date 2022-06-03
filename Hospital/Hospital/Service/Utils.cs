a01cda8750de74772883bda71cf35d8587d48763
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-06-03 19:05:41 +0200

    Added new class for validation

---
+++ Hospital/Hospital/Service/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Service
+{
+    class Utils
+    {
+        public static bool IsDateFormValid(string date)
+        {
+            DateTime checkDate;
+            bool validDate = DateTime.TryParseExact(date, "MM/dd/yyyy", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.None, out checkDate);
+            if (!validDate)
+            {
+                Console.WriteLine("Nevalidan unos datuma");
+                return false;
+            }
+            else if (checkDate <= DateTime.Now)
+            {
+                Console.WriteLine("Uneli ste datum koji je prosao ili je danasnji.");
+                return false;
+            }
+            return true;
+        }
+
+        public static bool IsTimeFormValid(string time)
+        {
+            TimeSpan checkTime;
+            bool validTime = TimeSpan.TryParse(time, out checkTime);
+            if (!validTime)
+            {
+                Console.WriteLine("Nevalidan unos vremena");
+                return false;
+            }
+            return true;
+        }
+    }
+}
