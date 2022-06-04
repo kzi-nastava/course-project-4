67e011717b385109a6cba2cdbc8312d5153be4ae
savic-a <anastasijas557@gmail.com>
2022-06-04 15:28:26 +0200

    Removed some methods

--- Hospital/Hospital/PatientImplementation/Patient.cs
+++ Hospital/Hospital/PatientImplementation/Patient.cs
@@ -13,7 +13,7 @@ namespace Hospital.PatientImplementation
     class Patient
     {
         private string _email;
-        private PatientAppointments _patientService;
+        private PatientAppointmentsService _patientService;
         private AppointmentService _appointmentService;
         private PatientSchedulingAppointment _patientScheduling;
         private PatientAnamnesis _patientAnamnesis;
@@ -24,6 +24,8 @@ namespace Hospital.PatientImplementation
         private UserService _userService;
         private UserActionService _userActionService;
         private DrugNotificationService _drugNotificationService;
+        private PatientModifyAppointment _modifyAppointment;
+        private RecommendedAppointment _recommendedAppointment;
 
         public string Email { get { return _email; } }
         public List<Appointment> PatientAppointments
@@ -33,21 +35,22 @@ namespace Hospital.PatientImplementation
         }
         public PatientDoctorSurvey PatientDoctorSurvey { get { return _doctorSurvey; } }
 
-        public Patient(string email, PatientSchedulingAppointment patientScheduling)
+        public Patient(string email)
         {
             this._email = email;
             this._appointmentService = new AppointmentService();
             this._drugNotificationService = new DrugNotificationService();
             this._userService = new UserService();
-            this._patientService = new PatientAppointments(this, _appointmentService);
-            this._patientScheduling = patientScheduling;
-            this._patientScheduling.RegisteredUser = this;
+            this._userActionService = new UserActionService(this);
+            this._patientService = new PatientAppointmentsService(this, _appointmentService);
+            this._patientScheduling = new PatientSchedulingAppointment(this, _appointmentService, _userService, _patientService, _userActionService);
             this._patientAnamnesis = new PatientAnamnesis(this, _appointmentService, _userService);
             this._currentAppointments = this._patientService.RefreshPatientAppointments();
-            this._doctorSearch = new PatientDoctorSearch(this, _userService);
+            this._doctorSearch = new PatientDoctorSearch(this, _userService, _patientScheduling);
             this._doctorSurvey = new PatientDoctorSurvey(_userService);
             this._drugNotification = new PatientDrugNotification(this, _appointmentService, _drugNotificationService);
-            this._userActionService = new UserActionService(this);
+            this._modifyAppointment = new PatientModifyAppointment(this, _patientService, _userActionService, _appointmentService);
+            this._recommendedAppointment = new RecommendedAppointment(this, _userService, _patientService, _userActionService, _patientScheduling, _appointmentService);
         }
 
         // methods
@@ -73,15 +76,15 @@ namespace Hospital.PatientImplementation
 
                 // patient choice
                 if (choice.Equals("1"))
-                    this.ReadOwnAppointments();
+                    this._patientService.ReadOwnAppointments();
                 else if (choice.Equals("2"))
-                    this.SchedulingAppointment("");
+                    this._patientScheduling.SchedulingAppointment("");
                 else if (choice.Equals("3"))
-                    this.UpdateOwnAppointment();
+                    this._modifyAppointment.UpdateOwnAppointment();
                 else if (choice.Equals("4"))
-                    this.DeleteOwnAppointment();
+                    this._modifyAppointment.DeleteOwnAppointment();
                 else if (choice.Equals("5"))
-                    this.RecommendationFreeAppointments();
+                    this._recommendedAppointment.RecommendationFreeAppointments();
                 else if (choice.Equals("6"))
                     this._patientAnamnesis.MainMenuForSearch();
                 else if (choice.Equals("7"))
@@ -98,101 +101,6 @@ namespace Hospital.PatientImplementation
             Console.WriteLine();
             Console.Write(String.Format("{0,3}|{1,10}|{2,10}|{3,10}|{4,10}|{5,10}|{6,10}|{7,10}",
                 "Br.", "Doktor", "Datum", "Pocetak", "Kraj", "Soba", "Tip", "Stanje"));
-        }
-
-        private void ReadOwnAppointments()
-        {
-            if (!_patientService.HasPatientAppointment(this._currentAppointments)) 
-                return;
-                
-            int serialNumber = 0;
-
-            this.TableHeaderForPatient();
-            Console.WriteLine();
-            
-            foreach (Appointment appointment in this._currentAppointments)
-            {
-                serialNumber++;
-                Console.WriteLine(serialNumber + ". " + appointment.DisplayOfPatientAppointment());
-            }
-            Console.WriteLine();
-        }
-
-        private void DeleteOwnAppointment() 
-        {
-            Appointment appointmentForDelete = _patientService.PickAppointmentForDeleteOrUpdate();
-
-            if (appointmentForDelete == null)
-                return;
-
-            this._patientService.DeleteAppointment(appointmentForDelete);
-            this._currentAppointments = _patientService.RefreshPatientAppointments();
-            this._userActionService.ActionRepository.AppendToActionFile("delete");
-            this._userActionService.AntiTrolMechanism();
-        }
-
-        private void UpdateOwnAppointment()
-        {
-            Appointment appointmentForUpdate = _patientService.PickAppointmentForDeleteOrUpdate();
-
-            if (appointmentForUpdate == null)
-                return;
-
-            string[] inputValues = _patientService.InputValuesForAppointment("");
-
-            if (!_patientScheduling.IsAppointmentFree(appointmentForUpdate.AppointmentId, inputValues))
-            {
-                Console.WriteLine("Nije moguce izmeniti pregled u izabranom terminu!");
-                return;
-            }
-
-            this._patientService.UpdateAppointment(appointmentForUpdate, inputValues);
-            this._userActionService.ActionRepository.AppendToActionFile("update");
-            this._userActionService.AntiTrolMechanism();
-        }
-
-        public void SchedulingAppointment(string doctorEmail)
-        {
-            string[] inputValues = _patientService.InputValuesForAppointment(doctorEmail);
-
-            if (!_patientScheduling.IsAppointmentFree("0", inputValues))
-            {
-                Console.WriteLine("Nije moguce zakazati pregled u izabranom terminu!");
-                return;
-            }
-
-            Appointment newAppointment = _patientScheduling.CreateAppointment(inputValues);
-            this._appointmentService.AddAppointment(newAppointment);
-            this._currentAppointments = _patientService.RefreshPatientAppointments();
-            this._userActionService.ActionRepository.AppendToActionFile("create");
-            this._userActionService.AntiTrolMechanism();
-
-            Console.WriteLine("Uspesno ste kreirali nov pregled!");
-        }
-
-        private void RecommendationFreeAppointments()
-        {
-            string[] inputValues = _patientService.InputValueForRecommendationAppointments();
-            string priority = _patientScheduling.CheckPriority();
-            Appointment newAppointment;
-
-            if (priority.Equals("1"))
-                newAppointment = _patientScheduling.FindAppointmentAtChosenDoctor(inputValues);
-            else
-                newAppointment = _patientScheduling.FindAppointmentInTheSelectedRange(inputValues);
-
-            if (newAppointment == null)
-            {
-                Console.WriteLine("Zakazivanje je odbijeno!");
-                return;
-            }
-            if (_patientScheduling.AcceptAppointment(newAppointment).Equals("2"))
-                return;
-
-            this._patientService.AppointmentService.AppendNewAppointmentInFile(newAppointment);
-            this._currentAppointments = _patientService.RefreshPatientAppointments();
-            this._userActionService.ActionRepository.AppendToActionFile("create");
-            this._userActionService.AntiTrolMechanism();
         }
 
         public void LogOut()
