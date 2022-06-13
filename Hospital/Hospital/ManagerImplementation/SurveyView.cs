f88d71855d4b1978df1f17be675e3477dde6566e
Tadija Šebez <sebez.sv4.2020@uns.ac.rs>
2022-06-13 15:37:26 +0200

    Implement view for doctor survey results

---
+++ Hospital/Hospital/ManagerImplementation/SurveyView.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Hospital.Service;
+using Hospital.Model;
+
+namespace Hospital.ManagerImplementation
+{
+    public class SurveyView
+    {
+        DoctorSurveyService _doctorSurveyService;
+
+        public SurveyView(DoctorSurveyService doctorSurveyService)
+        {
+            _doctorSurveyService = doctorSurveyService;
+        }
+
+        public void ViewSurveyResults()
+        {
+            Console.WriteLine("1. Rezultati anketa o bolnici");
+            Console.WriteLine("2. Rezultati anketa o doktorima");
+            Console.Write(">> ");
+            string choice = Console.ReadLine();
+
+            if (choice.Equals("1"))
+                ViewHospitalSurveyResults();
+            else if (choice.Equals("2"))
+                ViewDoctorSurveyResults();
+        }
+
+        private void ViewHospitalSurveyResults()
+        {
+
+        }
+
+        private void PrintDoctorSurveyResult(DoctorSurveyResult result)
+        {
+            Console.WriteLine("Ime: " + result.Doctor.Name);
+            Console.WriteLine("Prezime: " + result.Doctor.Surname);
+            Console.WriteLine("E-mail: " + result.Doctor.Email);
+            Console.WriteLine("Broj anketa: " + result.SurveyCount);
+            
+            Console.Write("Prosecna ocena kvaliteta: " + result.AverageQuality + " ->");
+            for (int i = 1; i <= 5; i++)
+                Console.Write(" " + i + "(" + result.QualityCount[i] + ")");
+            Console.WriteLine();
+            
+            Console.Write("Prosecna ocena preporuke: " + result.AverageRecommendation + " ->");
+            for (int i = 1; i <= 5; i++)
+                Console.Write(" " + i + "(" + result.RecommendationCount[i] + ")");
+            Console.WriteLine();
+
+            Console.WriteLine("Komentari");
+            Console.WriteLine("-----------------------");
+            foreach (string comment in result.Comments)
+            {
+                Console.WriteLine(comment);
+            }
+            Console.WriteLine();
+        }
+
+        private void PrintDoctors(List<DoctorSurveyResult> doctors)
+        {
+            foreach (DoctorSurveyResult doctor in doctors)
+            {
+                Console.WriteLine("Ime: " + doctor.Doctor.Name);
+                Console.WriteLine("Prezime: " + doctor.Doctor.Surname);
+                Console.WriteLine("E-mail: " + doctor.Doctor.Email);
+                Console.WriteLine("Prosecna ocena kvaliteta: " + doctor.AverageQuality);
+                Console.WriteLine();
+            }
+        }
+
+        private void PrintBestDoctors()
+        {
+            Console.WriteLine("Najbolje ocenjeni doktori");
+            Console.WriteLine("-------------------------");
+            List<DoctorSurveyResult> bestDoctors = _doctorSurveyService.GetBestDoctors();
+            PrintDoctors(bestDoctors);
+        }
+
+        private void PrintWorstDoctors()
+        {
+            Console.WriteLine("Najgore ocenjeni doktori");
+            Console.WriteLine("-------------------------");
+            List<DoctorSurveyResult> worstDoctors = _doctorSurveyService.GetWorstDoctors();
+            PrintDoctors(worstDoctors);
+        }
+
+        private void ViewDoctorSurveyResults()
+        {
+            List<DoctorSurveyResult> results = _doctorSurveyService.GetResults();
+            Console.WriteLine("Rezultati anketa o svim doktorima");
+            Console.WriteLine("---------------------------------");
+            
+            foreach (DoctorSurveyResult result in results)
+            {
+                PrintDoctorSurveyResult(result);
+            }
+
+            PrintBestDoctors();
+            PrintWorstDoctors();
+        }
+    }
+}
