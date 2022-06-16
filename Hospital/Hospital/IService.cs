91d65e064e07e1572867c820767360a240fb5b88
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-06-16 11:06:49 +0200

    Added service pattern interfaces

---
+++ Hospital/Hospital/IService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    public interface  IService<T> where T: class
+    {
+    }
+}
