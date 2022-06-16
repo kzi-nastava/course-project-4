776fe8bc1ca6afb4a31a33cc220d102dcf28a3f6
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2022-06-16 11:06:16 +0200

    Added repository pattern interfaces

---
+++ Hospital/Hospital/Appointments/Repository/IReferralRepository.cs
@@ -0,0 +1,13 @@
+﻿using Hospital.Appointments.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Appointments.Repository
+{
+   public interface  IReferralRepository:IRepository<Referral>
+    {
+    }
+}
